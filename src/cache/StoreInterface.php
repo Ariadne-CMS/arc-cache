05a8347f575a9db0d41e00447701239d14df6939
Auke van Slooten <auke@poefke.nl>
2014-06-25 18:55:26 +0200

    changed to PSR-2 coding style

--- src/cache/StoreInterface.php
+++ src/cache/StoreInterface.php
@@ -1,31 +1,31 @@
 <?php
 
-	/*
-	 * This file is part of the Ariadne Component Library.
-	 *
-	 * (c) Muze <info@muze.nl>
-	 *
-	 * For the full copyright and license information, please view the LICENSE
-	 * file that was distributed with this source code.
-	 */
-	 
-	namespace arc\cache;
-	
-	interface StoreInterface {
+    /*
+     * This file is part of the Ariadne Component Library.
+     *
+     * (c) Muze <info@muze.nl>
+     *
+     * For the full copyright and license information, please view the LICENSE
+     * file that was distributed with this source code.
+     */
 
-		public function cache( $name, $calculateCallback );
-		public function get( $name );
-		public function set( $name, $value, $timeout = 7200 );
-		public function getInfo( $name );
-		public function timeout( $timeout );
+namespace arc\cache;
 
-		public function isFresh( $path, $freshness = 0 );
-		public function getIfFresh( $path, $freshness = 0 );
+    interface StoreInterface
+    {
+        public function cache( $name, $calculateCallback );
+        public function get( $name );
+        public function set( $name, $value, $timeout = 7200 );
+        public function getInfo( $name );
+        public function timeout( $timeout );
 
-		public function lock( $name );
-		public function wait( $name );
-		public function unlock( $name );
+        public function isFresh( $path, $freshness = 0 );
+        public function getIfFresh( $path, $freshness = 0 );
 
-		public function remove( $name );
-		public function purge( $name = null );
-	}
+        public function lock( $name );
+        public function wait( $name );
+        public function unlock( $name );
+
+        public function remove( $name );
+        public function purge( $name = null );
+    }
